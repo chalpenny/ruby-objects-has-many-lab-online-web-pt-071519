63f6df89c085c1a174e321b8eb745c9733087123
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-14 03:12:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,12 +3,17 @@ class Author
   attr_accessor :name
 
   @@all = []
-
+    
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+    
+  end
   def add_post(post)
     post.author = self
   end
